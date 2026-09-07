089d37a0b04f32912425c40efb835eba9f273c05
alimdd <alimdd2014@gmail.com>
2026-09-07 15:49:46 +0300

    Fix Laravel 8 configuration and dependencies

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The list of the inputs that are never flashed to the session.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     */
+    public function report(Throwable $e)
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $e)
+    {
+        return parent::render($request, $e);
+    }
+}
